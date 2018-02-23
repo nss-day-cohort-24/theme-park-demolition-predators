069cb8bf919258e0ef292e95b9ad3e07b37cf1ea
Million Yemane <wedi_ngus@yahoo.com>
2018-02-23 06:12:21 -0600

    uptodated commit

--- js/load.js
+++ js/load.js
@@ -1,48 +1,85 @@
-"use strict";
-                    console.log("loading hi million .js");  //testing the file
-     
-     
-                let areas={}; //areas is defining
+        "use strict";
+        console.log("loading hi load.js");  //testing the file
 
-                // Initialize Firebase url and application key
-                function myurl() {
-                   return{ 
-                        databaseURL: "https://theme-park-dp.firebaseio.com",
-                        apiKey: "AIzaSyDSwR_6ryh4aah9J6XjlzuGTLHRzalW-_o"
+            var config = {
+                apiKey: "AIzaSyDSwR_6ryh4aah9J6XjlzuGTLHRzalW-_o",
+                authDomain: "theme-park-dp.firebaseapp.com",
+                databaseURL: "https://theme-park-dp.firebaseio.com",
+
+            };
+
+           // // Initialize Firebase
+        //   var firebase = require('lib\node_modules\firebase');
+            
+
+          //var app=firebase.initializeApp({config});
+          //var database = app.database();
+          //var databaseRef = database.ref().child('areas');
+     
+               
+    //searching attractions
+        function areas(){                                                   //take a method, and url
+           return new Promise(function (resolve,reject) {                    //take aresolve and reject
+                     var xhr = new XMLHttpRequest();                          //set the xmlhttp
+                 xhr.addEventListener('load', function () {                  // load for the following  
+                var areas = JSON.parse(this.responseText);
+             if (this.status >= 200 && this.status < 300) {                  // Holds the status of the XMLHttpRequest which it means ok 
+                resolve(areas);                                              //pass the responce
+                   } else {                                                  //otherwise
+                       reject({
+                         status: this.status,                               //page not found
+                         statusText: xhr.statusText                        //           
+                       });
+                     }
+                  xhr.onerror = function () {                        //  after it rejected we need to handle it
+                    reject({
+                      status: this.status,
+                      statusText: xhr.statusText
+                    });
                   };
-                }
-                //console.log(myurl);
-                
-                function getData(){                                    //take a method, and url
-                    return new Promise(function (resolve,reject) {     //take aresolve and reject
-                        var xhr = new XMLHttpRequest();                 //set the xmlhttp
-                        
-                    xhr.addEventListener('load', function () {              // load for the following  
-                        if (this.status >= 200 && this.status < 300) {      // Holds the status of the XMLHttpRequest which it means ok 
-                            // let areas = JSON.parse(data)
-                            resolve(areas);                                 //pass the responce
-                        } else {                                            //otherwise
-                            reject({
-                                status: this.status,                        //
-                                statusText: xhr.statusText                  //           
-                                });
-                            }
-                   
-                    xhr.onerror = function () {                        //  after it rejected we need to handle it
-                            reject({
-                                status: this.status,
-                                statusText: xhr.statusText
-                                });
-                            };
-                    var x = xhr.open("GET", "https://theme-park-dp.firebaseio.com/areas.json"); //get the data 
+                     var x = xhr.open("GET", "https://theme-park-dp.firebaseio.com/areas.json"); //get the data 
                             xhr.send();
                         });
                     });
-                    
                 }
                             
-        
-            
-            
+  //searching specific area(by id)
+
+        function getData(id) {                                                   //take a method, and url
+            return new Promise(function (resolve, reject) {                    //take aresolve and reject
+                var xhr = new XMLHttpRequest();                          //set the xmlhttp
+                xhr.addEventListener('load', function () {                  // load for the following  
+                    var areas = JSON.parse(this.responseText);
+                    if (this.status >= 200 && this.status < 300) {                  // Holds the status of the XMLHttpRequest which it means ok 
+                        resolve(areas);                                              //pass the responce
+                    } else {                                                  //otherwise
+                        reject({
+                            status: this.status,                        //
+                            statusText: xhr.statusText                  //           
+                        });
+                    }
+                    xhr.onerror = function () {                        //  after it rejected we need to handle it
+                        reject({
+                            status: this.status,
+                            statusText: xhr.statusText
+                        });
+                    };
+                    var x = xhr.open("GET", "https://theme-park-dp.firebaseio.com/areas.json"); //get the data 
+                    xhr.send();
+                });
+            });
+        }
+
+  //for each
+    getData('GET', 'http://jsonplaceholder.typicode.com/todos').then(function (data) {
+                (data.forEach((item)=>{
+                        alert(item.id);
+                });
+                console.log("thank you for visiting", data)
+            };
+            function(data){
+                alert("try Again Please");
+            });
       
-       module.exports=myurl;
+       module.exports=areas;
+       //module.exports =areas;
--- js/main.js
+++ js/main.js
@@ -2,5 +2,4 @@
 console.log("hello main.js");
 
 
-
 let db = require('./load');
