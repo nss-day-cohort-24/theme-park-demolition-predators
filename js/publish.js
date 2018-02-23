a8d4a0263c9a4f03ee49034f89204ac6ccdf064f
Sam Kimball <jibequan@gmail.com>
2018-02-23 13:22:04 -0600

    Tweaking buttons and layout

--- js/publish.js
+++ js/publish.js
@@ -14,14 +14,25 @@ let loader = require("./load");
 let areaGrid = () => {
     let areaDiv = document.getElementById("data-display");
     areaDiv.innerHTML =
-    `<button id="mainSt-btn">Main Street U.S.A</button>
-     <button id ="adventure-btn">Adventure Land</button>
-     <button id="frontier-btn">Frontier Land</button>
-     <button id="liberty-btn">Liberty Square</button>
-     <button id="fantasy-btn">Fantasy Land</button>
-     <button id="tomorrow-btn">Tomorrow Land</button>
-     <button id="cinderella-btn">Cinderella Castle</button>
-     <button id="allArea-btn">All Area</button>`;
+    `<div class="row">
+     <div id="mainSt-btn" class="col">Main Street U.S.A</div>
+     <div id="adventure-btn" class="col">Adventure Land</div>
+     </div>
+
+     <div class="row">
+     <div id="frontier-btn" class="col">Frontier Land</div>
+     <div id="liberty-btn" class="col">Liberty Square</div>
+     </div>
+
+     <div class="row">
+     <div id="fantasy-btn" class="col">Fantasy Land</div>
+     <div id="tomorrow-btn" class="col">Tomorrow Land</div>
+     </div>
+
+     <div class="row">
+     <div id="cinderella-btn" class="col">Cinderella Castle</div>
+     <div id="allArea-btn" class="col">All Area</div>
+     </div>`;
 };
 
 
