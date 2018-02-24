947871b4cc71ca60d172ef1f9cbe79503615958c
Dan Watson <danwatson00@yahoo.com>
2018-02-24 07:29:14 -0600

    more load.js

--- js/load.js
+++ js/load.js
@@ -1,40 +1,6 @@
 "use strict";
 console.log("loading hi load.js");  //testing the file
 
-//Search Function for Search Bar by Dan
-/* var getValue(obj, key) {
-    var objects = [];
-    for (var i in obj) {
-        if (!obj.hasOwnProperty(i)) continue;
-        if (typeof obj[i] == 'object') {
-            objects = objects.concat(getValues(obj[i], key));
-            }  else if (i == key) {
-                objects.push(obj[i]);
-            }
-    }
-    return objects;
-} */
-
-/* var results = [];
-var entry;
-var str = "";
-
-str = str.toUpperCase();
-
-function search(source, str) {
-    var results;
-
-    str = str.toUpperCase();
-    results = source.filter(function (entry) {
-        return entry.str.toUpperCase().indexOf(str) !== -1;
-    });
-    return results;
-} */
-
-
-
-//From Million, pull and filter data
-
 var config = {
     apiKey: "AIzaSyDSwR_6ryh4aah9J6XjlzuGTLHRzalW-_o",
     authDomain: "theme-park-dp.firebaseapp.com",
@@ -52,7 +18,14 @@ function fillTheData (data) {
         inventory.push(data[item]);
 });
     console.log("inventory after fill:", inventory);
+    return inventory;
 }
+
+//get inventory data
+dataObject.getInventory = () => {
+    console.log("getInventory", inventory);
+    return inventory;
+};
 
 function areas() { 
     console.log("areas function");                                                  //take a method, and url
@@ -72,6 +45,26 @@ function areas() {
             xhr.send();
     });
 }
+
+//load data xhr
+dataObject.loadInventory = () => {
+    let val = 1;
+    return new Promise(function (resolve, reject) {
+        let inventoryLoader = new XMLHttpRequest();
+
+        inventoryLoader.open("GET", `"https://theme-park-dp.firebaseio.com/areas.json?orderBy="typeId"&equalTo="${val}"`);
+       
+        inventoryLoader.send();
+
+        inventoryLoader.addEventListener("load", function () {
+            let data = JSON.parse(this.responseText);
+            console.log("data", data);
+            fillTheData(data);
+            console.log("inventory", inventory);
+            resolve(inventory);
+        });
+    });
+};
 
 //searching specific area(by id)
 
@@ -112,5 +105,35 @@ getData('GET', 'http://jsonplaceholder.typicode.com/todos')
         console.log("try Again Please");
     });
 
-module.exports = {areas};
+module.exports = {areas,dataObject};
       //module.exports =areas;
+
+      //Search Function for Search Bar by Dan
+/* var getValue(obj, key) {
+    var objects = [];
+    for (var i in obj) {
+        if (!obj.hasOwnProperty(i)) continue;
+        if (typeof obj[i] == 'object') {
+            objects = objects.concat(getValues(obj[i], key));
+            }  else if (i == key) {
+                objects.push(obj[i]);
+            }
+    }
+    return objects;
+} */
+
+/* var results = [];
+var entry;
+var str = "";
+
+str = str.toUpperCase();
+
+function search(source, str) {
+    var results;
+
+    str = str.toUpperCase();
+    results = source.filter(function (entry) {
+        return entry.str.toUpperCase().indexOf(str) !== -1;
+    });
+    return results;
+} */
