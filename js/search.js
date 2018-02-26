4ee8b9c5988963166f6f83a508cf92b51c15d7e6
Arthur Smith <arthursmith@ms-MacBook-Pro.local>
2018-02-26 12:53:50 -0600

    search bar does stuff

--- js/search.js
+++ js/search.js
@@ -2,11 +2,13 @@
 
 let searchBar = document.getElementById("txt-search");
 let areaDiv = document.getElementById("data-display");
+let searchTerm = "Meet";
+let btn = document.getElementById("printSearch");
 
 let searchXHRequest = () => { //making XHR function for search bar
     return new Promise((resolve, reject) => { //making promise
         let data = new XMLHttpRequest();//puting the XHR inside a variable named "data"
-        data.addEventListener('laod', function() { //adding event listener to parse data upon load
+        data.addEventListener('load', function() { //adding event listener to parse data upon load
             var themeParkJSON = JSON.parse(this.responseText);//puting parsed data into variable named themeParkJSON
             resolve(themeParkJSON);
         });
@@ -18,10 +20,33 @@ let searchXHRequest = () => { //making XHR function for search bar
     });
 };
 
-console.log("data?",searchXHRequest().then);
 
+function onEnter(){
+    searchXHRequest().then( 
+        (json) => {
+            console.log("json attractions", json.attractions);
+            
+            let currentSearch;
+            areaDiv.innerHTML = "";
+            console.log("are you there?");
+            
+            for (var i = 0; i < json.attractions.length; i++){
+                currentSearch = (json.attractions[i].name).toLowerCase();
+                let input = searchBar.value.toLowerCase();
+                if (currentSearch.includes(input)){
+                    //console.log("match", currentSearch);
+                    areaDiv.innerHTML += `<div id="attracion-name">${json.attractions[i].name}</div>`;
+                }
+            }
+        },
+        (reject) => {
+            console.log("SOMETHING WENT REALLY WRONG");
+        }
+    );
+}
 
+btn.addEventListener("click", onEnter);
 
+//onEnter();
 
-
-
+module.exports = {onEnter, searchXHRequest};
